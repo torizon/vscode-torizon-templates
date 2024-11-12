cb32c0b81f10067b6ab0796ddbcf78ae1d450152
Erik Tagirov <erik.tagirov@edgemtech.ch>
2024-11-12 18:53:45 +0100

    - set logo for template (temporary logo) - Update templates.json so that LVGL appears as a choice - Update tasks.json to fetch LVGL from github if needed - Simplify + cleanup CMakeLists.txt - hardcode to /dev/fb0 - Provide default configuration lv_conf.h - Update launch.json so that executable is debugged on the device

--- cLvgl/src/main.c
+++ cLvgl/src/main.c
@@ -1,63 +1,58 @@
-#include "lvgl/lvgl.h"
-#include "lvgl/demos/lv_demos.h"
+/*******************************************************************
+ *
+ * main.c - LVGL example application using LVGL on Unix-like
+ * operating systems.
+ *
+ * Based on lv_port_linux/main.c
+ *
+ * Copyright (c) 2024 LVGL LLC.
+ *
+ * Authors:
+ * LVGL LLC, gabriel.catel@edgemtech.ch
+ * LVGL LLC, erik.tagirov@edgemtech.ch
+ *
+ ******************************************************************/
 #include <unistd.h>
-#include <pthread.h>
-#include <time.h>
-#include <stdio.h>
 #include <stdlib.h>
 
-static const char *getenv_default(const char *name, const char *dflt)
-{
-    return getenv(name) ? : dflt;
-}
+#include "lvgl/lvgl.h"
+#include "lvgl/demos/lv_demos.h"
 
-#if LV_USE_EVDEV
-static void lv_linux_init_input_pointer(lv_display_t *disp)
-{
-    // Enables a pointer (touchscreen/mouse) input device
-    // Use 'evtest' to find the correct input device. /dev/input/by-id/ is recommeded if possible
-    // Use /dev/input/by-id/my-mouse-or-touchscreen or /dev/input/eventX
-    const char *input_device = getenv_default("LV_LINUX_EVDEV_POINTER_DEVICE", "/dev/input/by-id/my-mouse-or-touchscreen");
-    lv_indev_t *touch = lv_evdev_create(LV_INDEV_TYPE_POINTER, input_device); 
-    lv_indev_set_display(touch, disp);
-
-    // Disable this if you want no cursor
-    LV_IMAGE_DECLARE(mouse_cursor_icon); /*Declare the image source.*/
-    lv_obj_t * cursor_obj = lv_image_create(lv_screen_active()); /*Create an image object for the cursor */
-    lv_image_set_src(cursor_obj, &mouse_cursor_icon); /*Set the image source*/
-    lv_indev_set_cursor(touch, cursor_obj); /*Connect the image  object to the driver*/
-}
-#endif
+#define FRAME_BUFFER_DEV "/dev/fb0"
 
 static void lv_linux_disp_init(void)
 {
-    const char *device = getenv_default("LV_LINUX_FBDEV_DEVICE", "/dev/fb0");
+    const char *device = FRAME_BUFFER_DEV;
     lv_display_t * disp = lv_linux_fbdev_create();
-    
-    #if LV_USE_EVDEV
-    lv_linux_init_input_pointer(disp);
-    #endif
-    
+
     lv_linux_fbdev_set_file(disp, device);
 }
 
-
-int main(void)
+static void lv_linux_run_loop(void)
 {
+    uint32_t time_till_next;
+
+    /*Handle LVGL tasks*/
+    while(1) {
+        time_till_next = lv_timer_handler();
+        usleep(time_till_next);
+    }
+}
+
+int main(int argc, char **argv)
+{
+
+    /* Initialize LVGL. */
     lv_init();
 
-    /*Linux display device init*/
+    /* Initialize the configured backend */
     lv_linux_disp_init();
 
     /*Create a Demo*/
     lv_demo_widgets();
     lv_demo_widgets_start_slideshow();
 
-    /*Handle LVGL tasks*/
-    while(1) {
-        uint32_t idle_time = lv_timer_handler(); /*Returns the time to the next timer execution*/
-        usleep(idle_time * 1000);
-    }
+    lv_linux_run_loop();
 
     return 0;
 }
