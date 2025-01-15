6ee7224c9e3957965d139a129f1d8966eaa8bd7b
Matheus Castello <matheus.castello@toradex.com>
2025-01-15 20:12:47 -0300

    scripts: setup-xonsh-dev: Add the build pipx and twine
    Use the debian packages as much as possible to install the dependencies
    
    Signed-off-by: Matheus Castello <matheus.castello@toradex.com>

--- scripts/bash/setup-xonsh-dev.sh
+++ scripts/bash/setup-xonsh-dev.sh
@@ -9,7 +9,10 @@ sudo apt update
 sudo apt install -y \
     python3 \
     python3-pip \
-    python3-setuptools
+    python3-build \
+    python3-setuptools \
+    pipx \
+    twine
 
 # install the pylsp using pipx
 pipx install xonsh
@@ -25,12 +28,6 @@ pipx inject xonsh python-lsp-server
 pipx inject xonsh pylsp-rope
 # FIXME: mypy is not working with xonsh
 # pipx inject xonsh pylsp-mypy
-
-# to be able to publish the package to PyPI
-pipx install twine
-
-# install the apollox nodejs module as global
-# npm install -g apollox
 
 # inject into the .vscode/settings.json the home dir on the pylsp.executable
 # to make it work with the pylsp installed with pipx
